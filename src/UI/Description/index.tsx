79f1a4604c96cb3ff8ceaa7f56198addf0ee5531
Veklich.DA <Veklich.DA@parma.ru>
2020-02-15 18:52:24 +0300

    #ui-components added ts and classnames liab to project, created descripotion UI component

---
+++ src/UI/Description/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import cx from 'classnames';
+import './style.scss';
+
+const Title = ({ color, marign, fontSize, uppercase, underline, nowrap, className, children }) => {
+	const classNames = cx(
+		color ? `description__${color}` : 'description__black',
+		fontSize ? `description__fontSize_${fontSize}` : `description__fontSize_s`,
+		uppercase && 'description__uppercase',
+		nowrap && 'description__nowrap',
+		underline && 'description__underline',
+		className && className
+	);
+
+	return <p className={classNames}>{children}</p>;
+};
+
+export { Title };
